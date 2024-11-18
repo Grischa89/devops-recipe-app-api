967d7fa98a25933bf700557a238ef8a4fc5a13e4
Greg Demo <project.abramov@gmail.com>
2024-11-18 19:51:21 +0100

    init terraform

---
+++ infra/setup/main.tf
@@ -0,0 +1,29 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.23.0"
+    }
+  }
+
+  backend "s3" {
+    bucket = "devops-recipe-app-tf-state-2"
+    key    = "terraform-state-setup"
+    region = "us-east-1"
+    encrypt= true
+    dynamodb_table = "devops-recipe-app-tf-lock-2"
+  }
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region = "us-east-1"
+  default_tags {
+    tags = {
+        Environment = terraform.workspace
+        Project = var.project
+        contact = var.contact
+        ManageBy = "Terraform/setup"
+    }
+  }
+}
