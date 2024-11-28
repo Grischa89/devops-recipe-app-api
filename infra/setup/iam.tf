7da29f2c9053ee24a4e392e2b04d98bfbe3451e6
Greg Demo <project.abramov@gmail.com>
2024-11-28 13:32:18 +0100

    change s3 iam policy 3

--- infra/setup/iam.tf
+++ infra/setup/iam.tf
@@ -17,13 +17,13 @@ resource "aws_iam_access_key" "cd" {
 data "aws_iam_policy_document" "tf_backend" {
   statement {
     effect    = "Allow"
-    actions   = ["s3:GetObject", "s3:ListBucket", "s3:HeadObject", "ec2:DescribeAvailabilityZones", "s3:*", ]
+    actions   = ["s3:GetObject", "s3:ListBucket", "s3:HeadObject", "ec2:DescribeAvailabilityZones", ]
     resources = ["arn:aws:s3:::${var.tf_state_bucket}"]
   }
 
   statement {
     effect  = "Allow"
-    actions = ["s3:GetObject", "s3:PutObject", "s3:DeleteObject", "s3:HeadObject", "s3:ListBucket", "ec2:DescribeAvailabilityZones", "s3:*", ]
+    actions = ["s3:GetObject", "s3:PutObject", "s3:DeleteObject", "s3:HeadObject", "s3:ListBucket", "ec2:DescribeAvailabilityZones", ]
     resources = [
       "arn:aws:s3:::${var.tf_state_bucket}/tf-state-deploy/*",
       "arn:aws:s3:::${var.tf_state_bucket}/tf-state-deploy-env/*"
@@ -137,7 +137,6 @@ data "aws_iam_policy_document" "ec2" {
       "ec2:DescribeAvailabilityZones",
       "s3:HeadObject",
       "s3:ListBucket",
-      "s3:*",
     ]
     resources = ["*"]
   }
