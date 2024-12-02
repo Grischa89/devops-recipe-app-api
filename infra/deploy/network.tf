751f47389e93be20c38a245097ce257fb2d13f60
Greg Demo <project.abramov@gmail.com>
2024-12-02 12:51:49 +0100

    added private subnets

--- infra/deploy/network.tf
+++ infra/deploy/network.tf
@@ -93,3 +93,30 @@ resource "aws_route" "public_internet_access_b" {
   destination_cidr_block = "0.0.0.0/0"
   gateway_id             = aws_internet_gateway.main.id
 }
+
+############################################
+# Private Subnets for internal access only #
+############################################
+resource "aws_subnet" "private_a" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.1.10.0/24"
+  # availability_zone = "${data.aws_region.current.name}a"
+  availability_zone = data.aws_availability_zones.available.names[0]
+
+  tags = {
+    # Name = "${local.prefix}-private-a"
+    Name = "${local.prefix}-${data.aws_availability_zones.available.names[0]}"
+  }
+}
+
+resource "aws_subnet" "private_b" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.1.11.0/24"
+  # availability_zone = "${data.aws_region.current.name}b"
+  availability_zone = data.aws_availability_zones.available.names[1]
+
+  tags = {
+    # Name = "${local.prefix}-private-b"
+    Name = "${local.prefix}-${data.aws_availability_zones.available.names[1]}"
+  }
+}
