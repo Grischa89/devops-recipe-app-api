e11c7c1f8890b2d8f22c4b0ea135c1b0cdca91fc
Greg Demo <project.abramov@gmail.com>
2024-12-15 15:41:41 +0100

    git drop description

--- infra/deploy/database.tf
+++ infra/deploy/database.tf
@@ -24,7 +24,6 @@ resource "aws_security_group" "rds" {
     from_port = 5432
     to_port   = 5432
 
-    # only these security_groups have incound access
     security_groups = [
       aws_security_group.ecs_service.id
     ]
