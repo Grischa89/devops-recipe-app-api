d7ab8486f0245f7b8038f7d2426be09a0d1ddd26
Greg Demo <project.abramov@gmail.com>
2025-01-03 22:02:19 +0100

    added app container defition

--- infra/deploy/ecs.tf
+++ infra/deploy/ecs.tf
@@ -35,6 +35,7 @@ resource "aws_iam_role_policy_attachment" "task_ssm_policy" {
   role       = aws_iam_role.app_task.name
   policy_arn = aws_iam_policy.task_ssm_policy.arn
 }
+
 resource "aws_cloudwatch_log_group" "ecs_task_logs" {
   name = "${local.prefix}-api"
 }
@@ -54,6 +55,54 @@ resource "aws_ecs_task_definition" "api" {
 
   container_definitions = jsonencode(
     [
+      {
+        name              = "api"
+        image             = var.ecr_app_image
+        essential         = true
+        memoryReservation = 256
+        user              = "django-user"
+        environment = [
+          {
+            name  = "DJANGO_SECRET_KEY"
+            value = var.django_secret_key
+          },
+          {
+            name  = "DB_HOST"
+            value = aws_db_instance.main.address
+          },
+          {
+            name  = "DB_NAME"
+            value = aws_db_instance.main.db_name
+          },
+          {
+            name  = "DB_USER"
+            value = aws_db_instance.main.username
+          },
+          {
+            name  = "DB_PASS"
+            value = aws_db_instance.main.password
+          },
+          {
+            name  = "ALLOWED_HOSTS"
+            value = "*"
+          }
+        ]
+        mountPoints = [
+          {
+            readOnly      = false
+            containerPath = "/vol/web/static"
+            sourceVolume  = "static"
+          }
+        ],
+        logConfiguration = {
+          logDriver = "awslogs"
+          options = {
+            awslogs-group         = aws_cloudwatch_log_group.ecs_task_logs.name
+            awslogs-region        = data.aws_region.current.name
+            awslogs-stream-prefix = "api"
+          }
+        }
+      },
       {
         name              = "proxy"
         image             = var.ecr_proxy_image
